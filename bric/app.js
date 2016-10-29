489cdde92d650acfea3e23d1b1b9a9014a68d021
Raul Alvarez <raulito2394@gmail.com>
2016-10-28 23:48:35 -0400

    database made

--- bric/app.js
+++ bric/app.js
@@ -5,13 +5,30 @@ var app = express();
 var bodyParser  = require('body-parser');
 var path = require('path');
 var stormpath = require('express-stormpath');
-var MongoClient = require('mongodb').MongoClient;
+// var MongoClient = require('mongodb').MongoClient;
+var mongoose = require('mongoose');
 var routes = require('./routes/index');
 
+// MongoClient.connect("mongodb://localhost:27017/bric", function(err, db) {
+//   if(!err) {
+//     console.log("Bric is connected");
+//   }
+// });
+
+var Schema = mongoose.Schema, ObjectId = Schema.ObjectId;
+mongoose.connect('mongodb://localhost/bric', function(err, res){
+	if(!err){
+		console.log("Connected to database!");
+	}
+	else{
+		throw err;
+	}
+});
 
 app.set('view engine','ejs');
 app.set('views', path.join(__dirname,'views'));
-app.use(express.static('./public'));
+
+app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.use('/', routes);
