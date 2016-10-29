dc610e77826f556b22537f2e6b8cfb8929484c6d
staceysze <staceysze@gmail.com>
2016-10-29 01:10:27 -0400

    Login/Register

--- app.js
+++ app.js
@@ -8,18 +8,32 @@ var stormpath = require('express-stormpath');
 var MongoClient = require('mongodb').MongoClient;
 var routes = require('./routes/index');
 
-
 app.set('view engine','ejs');
 app.set('views', path.join(__dirname,'views'));
 app.use(express.static('./public'));
+app.use(stormpath.init(app, {
+    apiKey: {
+        id: process.env.STORMPATH_CLIENT_APIKEY_ID,
+        secret: process.env.STORMPATH_CLIENT_APIKEY_SECRET
+    }
+}));
 app.use(bodyParser.urlencoded({extended: false}));
-
 app.use('/', routes);
 
-app.get('/', function(req, res) {
-    res.render('home');
-});
+// app.get('/', function(req, res) {
+//     res.render('home');
+// });
 
-app.listen(8000, function(){
-    console.log("Running on 8000...");
+
+
+
+
+
+
+
+app.on('stormpath.ready', function () {
+    console.log("Stormpath ready!");
+    app.listen(8000, function () {
+        console.log("Running on 8000...");
+    });
 });
