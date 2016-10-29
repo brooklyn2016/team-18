b3bea2b75f9e4b5521002e0358a965947ff7fee6
Tarek <tareksaidee@hotmail.com>
2016-10-29 11:21:28 -0400

    fix check fields for login

--- testing/app/src/main/java/com/example/tarek/testing/logInPage.java
+++ testing/app/src/main/java/com/example/tarek/testing/logInPage.java
@@ -4,17 +4,28 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class logInPage extends AppCompatActivity {
+
+    EditText username;
+    EditText password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in_page);
+        username = (EditText) findViewById(R.id.emailAddress);
+        password = (EditText) findViewById(R.id.password_enter);
     }
 
-    public void goToUpload(View view){
-        Intent intent = new Intent(this,MainActivity.class);
-        startActivity(intent);
+    public void goToUpload(View view) {
+        if (username.getText().toString().equals("") || password.getText().toString().equals(""))
+            Toast.makeText(this, "Some fields are empty", Toast.LENGTH_SHORT).show();
+        else {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        }
     }
 }
